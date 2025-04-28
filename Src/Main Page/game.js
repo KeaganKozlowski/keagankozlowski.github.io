f6dbf77370d0667f9eb55820ce778f45132845df
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2025-04-28 11:43:04 +0100

    adding animation to counter

--- Src/Main Page/game.js
+++ Src/Main Page/game.js
@@ -4,6 +4,10 @@ function increase(){
         let currentAmount = parseInt(currencyAmount.textContent, 10) || 0; // Get current amount or default to 0
         currentAmount += 1; // Increment by 1
         currencyAmount.textContent = currentAmount; // Update the displayed amount
+        currencyAmount.classList.add('up');
+        setTimeout(() =>{
+            currencyAmount.classList.remove('up')
+        }, 500)
     } else {
         console.error('Element with ID "currency-amount" not found.');
     }
